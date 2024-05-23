228376751d10e759cff90ab8d01fd30bf5fe95dd
StaphoneWizzoh <95640951+StaphoneWizzoh@users.noreply.github.com>
2024-05-23 10:58:12 +0300

    Passing props to component

--- pages/index.vue
+++ pages/index.vue
@@ -4,19 +4,22 @@
         <div
             class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-auto h-auto"
         >
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            <ProductCard
+                v-for="product in products"
+                :key="product.id"
+                :id="product.id"
+                :name="product.name"
+                :price="product.price"
+                :imgUrl="product.imgUrl"
+                :quantity="products.quantity"
+            />
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { useProductStore } from "#imports";
+
+const productStore = useProductStore();
+const products = productStore.allProducts;
 </script>
