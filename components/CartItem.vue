4774f2e8cf53977767ed366fa6c946936c9df60b
StaphoneWizzoh <95640951+StaphoneWizzoh@users.noreply.github.com>
2024-05-23 13:01:15 +0300

    cartItem for chechout page

---
+++ components/CartItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex items-center">
+        <img src="https://picsum.photos/200/300" alt="Product" class="h-20" />
+        <div class="flex items-center">
+            <p>{{ name }}</p>
+            <Icon
+                name="material-symbols:remove"
+                size="24"
+                color="#000"
+                class="cursor-pointer"
+            />
+            <p>1</p>
+            <Icon
+                name="material-symbols:add-rounded"
+                size="24"
+                color="#000"
+                class="cursor-pointer"
+            />
+            <p>KSH {{ price }}</p>
+            <Icon
+                name="streamline:interface-delete-2-remove-bold-add-button-buttons-delete"
+                size="24"
+                color="#000"
+                class="cursor-pointer"
+                @click="cartStore.removeItem(id)"
+            />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useCartStore } from "#imports";
+
+const cartStore = useCartStore();
+
+defineProps(["id", "name", "price", "imgUrl", "quantity"]);
+</script>
